3be6985c3de7ee107a5d916a8f1aec3116c64b95
said <saidergashevtashkent@gmail.com>
2025-10-03 11:08:51 +0500

    Теперь счёт сохраняется в отдельном файле, и загружается при запуске программы

--- Bank.py
+++ Bank.py
@@ -1,33 +1,35 @@
+import pickle
+from typing import BinaryIO
+
 class Bank:
     def __init__(self, check):
-        self.check = check
+        self.__check = check
 
     def show_check(self):
-        print(f'Нынешнее состояние вашего счёта: {self.check}\n')
+        return self.__check
 
-    def account_replenishment(self):
+    def replenishment(self):
         while True:
             try:
                 sum_to_addition = int(input('Введите сумму которую хотите закинуть на счёт: '))
-                self.check += sum_to_addition
+                self.__check += sum_to_addition
                 print('Операция прошла успешно!\n')
                 break
 
             except ValueError:
                 print('Не верный ввод!\n')
 
-    def withdrawal_account(self):
+    def withdrawal(self):
         while True:
             try:
                 sum_to_removale = int(input('Введите сумму которую хотите снять: '))
-                if sum_to_removale > self.check:
+                if sum_to_removale > self.__check:
                     print('На счету недостаточно средств!\n')
-                    break
 
-                elif sum_to_removale == self.check:
+                elif sum_to_removale == self.__check:
                     choice = input('Вы точно хотите снять все средства: ').lower()
                     if choice == 'да':
-                        self.check -= sum_to_removale
+                        self.__check -= sum_to_removale
                         print('Операция прошла успешно!\n')
                         break
 
@@ -38,34 +40,44 @@ class Bank:
                     else:
                         print('Не известная команда!\n')
 
-                elif sum_to_removale <= self.check:
-                    self.check -= sum_to_removale
+                elif sum_to_removale <= self.__check:
+                    self.__check -= sum_to_removale
                     print('Операция прошла успешно!\n')
                     break
 
             except ValueError:
                 print('Не верный ввод!\n')
 
-    def __str__(self):
-        return 'Это класс Bank!'
 
-user_check = Bank(0)
+FILENAME = 'file.dat'
+check_now = 0
+
+try:
+    with open(FILENAME, 'rb') as file:
+        check_now = pickle.load(file)
+except (FileNotFoundError, EOFError):
+    print('Файл не найден!')
+
+user_check = Bank(check_now)
 
 while True:
     user_choice = input('Введите операцию (Вывод/Выход/Снятие/Пополнение): ').lower()
 
-    if user_choice == 'вывод' or user_choice == '1':
-        user_check.show_check()
+    if user_choice in ('вывод', '1'):
+        print(user_check.show_check())
 
-    elif user_choice == 'выход' or user_choice == '2':
+    elif user_choice in ('выход', '2'):
+        with open(FILENAME, 'wb') as file:
+            pickle.dump(user_check.show_check(), file)
+
         print('До свидания!')
         break
 
-    elif user_choice == 'снятие' or user_choice == '3':
-        user_check.withdrawal_account()
+    elif user_choice in ('снятие', '3'):
+        user_check.withdrawal()
 
-    elif user_choice == 'пополнение' or user_choice == '4':
-        user_check.account_replenishment()
+    elif user_choice in ('пополнение', '4'):
+        user_check.replenishment()
 
     else:
         print('Не извесная операция!\n')
